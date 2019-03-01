c8ba025cfcb15448f71dfc6d5312b3cbd5df3213
olb1605 <olb1605@gmail.com>
2019-03-01 15:47:40 +0100

    Commit adjust

--- R/earthquake_clean_data.R
+++ R/earthquake_clean_data.R
@@ -1,7 +1,3 @@
-####
-#if(getRversion()>='2.15.1')
-#  utils::globalVariables(c("LOCATION_NAME","I_D","YEAR","MONTH","DAY","LATITUDE","LONGITUDE",
-#                           "EQ_PRIMARY","COUNTRY","STATE","TOTAL_DEATHS","DATE","YEAR4"))
 #' @title Function to cleans NOAA earthquake and localisation data
 #'
 #'
